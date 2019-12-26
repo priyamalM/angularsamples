b3d8b143b5fa4db6de53096256fd72ecf68c7d9e
priyamal <priyamal@priyamals-MacBook-Pro.local>
2019-12-26 20:58:53 +0530

    added template driven form snippet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplatedrivenComponent } from './templatedrivenform/templatedriven/templatedriven.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplatedrivenComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
